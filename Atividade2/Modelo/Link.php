2459f6107203fbca8ebffa425dc1c7c84d37f5f3
Mayara Navakoviski <mayara.navakoviski.tds2023@gmail.com>
2025-04-04 15:14:59 -0300

    Update Link.php

--- Atividade2/Modelo/Link.php
+++ Atividade2/Modelo/Link.php
@@ -6,6 +6,12 @@ class Link {
     private $linkImg;
     private $info;
 
+    
+    public function __construct($linkImg, $info) {
+        $this->linkImg = $linkImg;
+        $this->info = $info;
+    }
+    
     // GETs e SETs:
 
     public function getLinkImg() {
